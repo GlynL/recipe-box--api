3aef3f9fc3e8555c009a81ff6884eb6215828f93
Glyn L <glyn_92@hotmail.com>
2018-10-03 11:36:37 +1000

    added helmet & cors

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 require("dotenv").config();
 const express = require("express");
+const helmet = require("helmet");
 const app = express();
+const cors = require("cors");
 const mongoose = require("mongoose");
 const PORT = process.env.PORT || 8080;
 const recipes = require("./routes/recipes");
 const users = require("./routes/users");
 
+// {
+//   contentSecurityPolicy: {
+//     defaultSrc: ["'self'"]
+//   }
+// }
+app.use(helmet());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    optionsSuccessStatus: 200
+  })
+);
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
